9e119116bc34470fa1e08729c8eb6ea40d3d5548
MatsCornegoor <m.cornegoor@gmail.com>
2020-07-31 11:38:35 +0200

    last changes before test build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,9 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est deleniti error officia autem, ipsa laudantium dignissimos ut odit temporibus saepe labore ipsum quos tempore animi at iste expedita suscipit natus quidem tenetur explicabo voluptatem minima. Voluptas exercitationem perspiciatis iusto doloribus, iure harum quod voluptatibus explicabo culpa quas temporibus reprehenderit illo sunt consectetur a, debitis distinctio, magnam quos. Facilis, doloribus nobis!hey</p>
 
     <div v-for="project in projects" :key="project">
-      <router-link :to="{ name: 'Projects', params: { id: project.id }}">{{project.name}}</router-link>
+      <!-- <router-link :to="{ name: 'Projects', params: { id: project.name }}">{{project.name}}</router-link> -->
+
+      <router-link v-bind:to="'/projects/' + project.id ">{{project.name}}</router-link>
     </div>
   </div>
 </template>
