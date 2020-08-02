02fb62fe860fa973fac045b1756bfc4946a2080b
MatsCornegoor <m.cornegoor@gmail.com>
2020-08-02 21:51:26 +0200

    cleanup before github push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home page">
+  <div class="page">
     <h1>hey, this is the home page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est deleniti error officia autem, ipsa laudantium dignissimos ut odit temporibus saepe labore ipsum quos tempore animi at iste expedita suscipit natus quidem tenetur explicabo voluptatem minima. Voluptas exercitationem perspiciatis iusto doloribus, iure harum quod voluptatibus explicabo culpa quas temporibus reprehenderit illo sunt consectetur a, debitis distinctio, magnam quos. Facilis, doloribus nobis!hey</p>
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="page">
-    <!-- <h1>{{id}}</h1> -->
     <h1> {{project.name}} </h1>
-    <!-- <p> {{project.created_on}} </p> -->
     <img v-bind:src="projectMedia.full_url" /> 
     <p> {{project.description}} </p>
 
@@ -11,7 +9,6 @@
 
 <script>
   import api from "../apiData.js"
-  // let imageId
   export default {
     props: ['id'],
 
